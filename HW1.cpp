fb6f66987d3019e0d2dae78338042e8e30877526
Alex <79256447+AlexKorshun@users.noreply.github.com>
2023-03-18 18:57:16 +0700

    create HW1

---
+++ HW1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    setlocale(LCC_ALL, "Russian");
+    for(int i = 0 ; i < 10 ; ++i>){
+        cout << "Hello World !!!1" << endl;
+    }
+}
