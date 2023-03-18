7e9a11829d27e5400264c7c7a762e4ddb555994b
Alex <79256447+AlexKorshun@users.noreply.github.com>
2023-03-18 12:02:18 +0000

    debug

--- HW1.cpp
+++ HW1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    for(int i = 0 ; i < 10 ; ++i>){
+    for(int i = 0 ; i < 10 ; ++i){
         cout << "Hello World !!!1" << endl;
     }
 }
